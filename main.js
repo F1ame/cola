6d744a9aedf546bc53bfb83c48abf717f779e8d4
F1ame <keeperflame45@gmail.com>
2017-06-21 23:53:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function loadd(pg){
+document.open();
+document.write(LOADING);
+document.close();
+document.open();
+document.write(topp + navbar + pg);
+document.close();
+}
+	function hov() {
+	document.getElementById("rick").src = 'https://f1ame.github.io/ricka/index.html'
+	}
---
+++ sh.js
@@ -0,0 +1,6 @@
+LOADING = '<img src="http://orig06.deviantart.net/df77/f/2013/094/8/d/loading_logofinal_by_zegerdon-d60eb1v.gif" alt="loading" height"="350" width="350" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%,-50%);">';
+sstyle = "<style>body{ background-color: rgb(70,130,180)}ul {    list-style-type: none;    margin: 0;    padding: 0;    overflow: hidden;    background-color: rgb(65,105,225);}li {    float: left;}li a {    display: block;    color: white;    text-align: center;    padding: 14px 16px;    text-decoration: none;}/* Change the link color to #111 (black) on hover */li a:hover {    background-color: rgb(100,149,237);}</style>"
+topp = "<!doctype html><head>"+ sstyle+ "<script src='sh.js'></script><script src='main.js'></script></head><body>";
+homepage = '<h1>this is the placeholder homepage</h1>';
+colapage = '<iframe style="width:100%; height: 720px;" id="rick" src=""></iframe><script>hov()</script>';
+navbar = "<ul><li><a onclick='loadd(homepage);'>Home</a></li><li><a onclick='loadd(colapage);'>Cola</a></li></ul>";
